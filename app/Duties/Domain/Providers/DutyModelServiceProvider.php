c3b64b14bb0b8df47fe20c371a4c097eb48b6889
Nesma Hamdy <nesma.cis2010@gmail.com>
2020-01-09 01:01:59 +0200

    change file location

---
+++ app/Duties/Domain/Providers/DutyModelServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Duties\Domain\Providers;
+
+use App\Duties\Domain\Models\Duty;
+use App\Duties\Domain\Observers\DutyObserver;
+use Illuminate\Support\ServiceProvider;
+
+class DutyModelServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Duty::observe(DutyObserver::class);
+    }
+}
